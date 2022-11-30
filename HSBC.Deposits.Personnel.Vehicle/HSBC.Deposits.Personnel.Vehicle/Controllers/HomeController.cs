61cf6777d3a4612f4b0ae0a4153f6cec95ce26f2
muraliguru2016@gmail.com <muraliguru2016@gmail.com>
2022-11-30 14:41:35 +0000

    names changed

--- HSBC.Deposits.Personnel.Vehicle/HSBC.Deposits.Personnel.Vehicle/Controllers/HomeController.cs
+++ HSBC.Deposits.Personnel.Vehicle/HSBC.Deposits.Personnel.Vehicle/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace HSBC.Deposits.Personnel.Vehicle.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            List<string> arrNames = new List<string>() { "asfsf", "asdfdsf" };
+            List<string> arrNames = new List<string>() { "Murali", "Krishna" };
             return StatusCode(StatusCodes.Status200OK, arrNames);
         }
 
